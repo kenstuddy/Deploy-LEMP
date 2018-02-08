38a8c2742264c79e711ddf6c9e76b7c13ddb15e0
Ken Studdy <kenstuddy@users.noreply.github.com>
2018-02-07 20:36:14 -0500

    Updated capitalization of comments

--- deploylemp.sh
+++ deploylemp.sh
@@ -4,7 +4,7 @@ then
     echo "Please run this script as root."
     exit
 fi
-#we can run this script as root, but we do not want to put the username as root
+#We can run this script as root, but we do not want to put the username as root
 until [[ $username != "" && $username != root ]]; do
 read -p "Please enter your username: " username
 done
@@ -14,7 +14,7 @@ sudo ufw allow 443
 sudo apt-get install openssh-server nginx apache2-utils mysql-server php7.0 php7.0-curl php7.0-cgi php-fpm php-mcrypt php-mysql php-dom php-mbstring php-zip unzip -y
 sudo apt-get install php7.0-xml php7.0-zip libxslt1.1 -y
 sudo mysql_secure_installation
-#done with installing LAMP, now it is time to secure the server
+#Done with installing LAMP, now it is time to secure the server
 sudo apt-get install fail2ban psad rkhunter chkrootkit -y
 sudo groupadd admin
 sudo usermod -a -G admin $username
